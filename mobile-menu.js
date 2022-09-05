a0f401e72196fc906738611bf2c7211dd7fd381d
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-09-05 16:00:42 +0200

    Add interactive menu to mobile version

---
+++ mobile-menu.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close-btn")
+const nav = document.querySelector(".top-nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.add("menu-show");
+})
+
+closeBtn.addEventListener("click", () => {
+    nav.classList.remove("menu-show");
+})

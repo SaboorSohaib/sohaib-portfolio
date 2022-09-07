ea8f47ccbec4f0c6afea4d471da487737ffe9e3e
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-09-07 18:39:52 +0430

    Add varible for form validation

---
+++ form-validation.js
@@ -0,0 +1,34 @@
+const submitBtn = document.querySelector('#submit-btn');
+const validEmail = (mail) => {
+    if (mail.match("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/")){
+        return true;
+    }
+    return false;
+};
+
+submitBtn.addEventListener('click', (event) => {
+    const validatedEmail = document.getElementById('mail').value;
+    if(!validEmail(validatedEmail)){
+        document.querySelector('#mail-message').innerHTML= "Please type the email in lowercase";
+        event.preventDefault();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

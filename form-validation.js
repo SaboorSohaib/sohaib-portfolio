90f27f22534f5032031ef89e55283e05a827635e
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-09-10 21:31:53 +0430

    Add some changes

--- form-validation.js
+++ form-validation.js
@@ -1,13 +1,13 @@
 // ======================= Form Validation ===================================
 
 const submitBtn = document.querySelector('#submit-btn');
-const validEmail = (mail) => {
+const validEmail = (email) => {
   if (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)) {
     return true;
   }
   return false;
 };
-submitBtn.addEventListener('click', (event) => {
+submitBtn.addEventListener('click', () => {
   const validatedEmail = document.getElementById('email').value;
   if (!validEmail(validatedEmail)) {
     document.querySelector('.lable').innerHTML = 'Please type the email in lowercase';

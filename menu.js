93795d4a166445cdf69590d5ae94caa1b5a10ad2
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-09-12 17:52:24 +0430

    Add interactive mobile menu

--- menu.js
+++ menu.js
@@ -1,3 +1,16 @@
+const menuBtn = document.querySelector('.menu');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelector('.top-nav');
 
-var name = 'abid';
-console.log(name);
+menuBtn.addEventListener('click', () => {
+  nav.classList.add('menu-show');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('menu-show');
+});
+
+document.querySelector('.menu').addEventListener('click', () => {
+  nav.style.display = 'block';
+});
+

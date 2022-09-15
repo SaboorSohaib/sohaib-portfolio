6c6dcbafa3e9761a7286c29f9ba9fd2c40252bc3
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-09-15 14:51:38 +0430

    Add functiom for from validation.

---
+++ Validation.js
@@ -0,0 +1,9 @@
+// ======================= Form Validation ===================================
+
+const submitBtn = document.querySelector('#submit-btn');
+const validEmail = (email) => {
+  if (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)) {
+    return true;
+  }
+  return false;
+};
